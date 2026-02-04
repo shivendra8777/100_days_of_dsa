fc48fc451b4fec8ca5321110f645ab66cb213e7e
Shivendra singh chauhan <shivendrasinghchauhan@Shivendras-MacBook-Air.local>
2026-02-04 20:40:56 +0530

    Day4

---
+++ Day_004.c
@@ -0,0 +1,47 @@
+/*Problem: Given an array of n integers, reverse the array in-place using two-pointer approach.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the reversed array, space-separated
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    // Input size of array
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Two-pointer approach
+    int left = 0;
+    int right = n - 1;
+
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    // Output reversed array
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
