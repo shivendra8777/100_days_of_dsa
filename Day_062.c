b43b21adf946c5636a65fabc02b01b6a39fcbb4f
Shivendra singh chauhan <shivendrasinghchauhan@Shivendras-MacBook-Air.local>
2026-04-03 19:55:57 +0530

    Day62

---
+++ Day_062.c
@@ -0,0 +1,66 @@
+/*Problem: Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+
+Input:
+- n (vertices)
+- m (edges)
+- edges (u, v)
+
+Output:
+- List of adjacency lists for each vertex*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge
+void addEdge(struct Node* adj[], int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+}
+
+// Print graph
+void printGraph(struct Node* adj[], int n) {
+    for (int i = 0; i < n; i++) {
+        struct Node* temp = adj[i];
+        printf("%d: ", i);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Node* adj[n];
+    
+    for (int i = 0; i < n; i++)
+        adj[i] = NULL;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        addEdge(adj, u, v);
+        addEdge(adj, v, u); // remove if directed graph
+    }
+
+    printGraph(adj, n);
+    return 0;
+}
